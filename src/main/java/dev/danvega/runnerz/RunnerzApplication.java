bbfc7566dd8bec73f9ade538cecb57238fdca64e
Matthew Bustamante <mbus7337@gmail.com>
2025-11-20 15:54:18 -0700

    Added Logging

--- src/main/java/dev/danvega/runnerz/RunnerzApplication.java
+++ src/main/java/dev/danvega/runnerz/RunnerzApplication.java
@@ -1,13 +1,18 @@
 package dev.danvega.runnerz;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RunnerzApplication {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
+		log.info("Application started successfully!");
 	}
 
 }
