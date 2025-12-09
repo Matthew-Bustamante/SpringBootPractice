f5e560ae9f9c399e0e6a5d808fd76342a09aee82
Matthew Bustamante <mbus7337@gmail.com>
2025-12-09 16:21:54 -0700

    Implemented CRUD methods and added validation

--- src/main/java/dev/danvega/runnerz/run/Run.java
+++ src/main/java/dev/danvega/runnerz/run/Run.java
@@ -2,11 +2,22 @@ package dev.danvega.runnerz.run;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 public record Run (
-		Integer id, 
+		Integer id,
+		@NotEmpty
 		String title, 
 		LocalDateTime startedOn, 
-		LocalDateTime completedOn, 
+		LocalDateTime completedOn,
+		@Positive
 		Integer Miles, 
 		Location loaction
-) {}
+) {
+	public Run {
+		if (!completedOn.isAfter(startedOn)) {
+			throw new IllegalArgumentException("Completed On Must be After Started On");
+		}
+	}
+}
